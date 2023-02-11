a1c8d6d9d1e78b0c1e90141f5466db27aa659655
Shi06vani <103645374+Shi06vani@users.noreply.github.com>
2023-02-11 11:40:41 +0700

    Add files via upload

---
+++ tempreact/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Homepage from './component/Homepage';
+import Aboutpage from './component/Aboutpage';
+import Footer from './component/Footer';
+
+
+function App() {
+  return (
+   <>
+   {/* <Homepage/> */}
+   <Aboutpage/>
+   {/* <Footer/> */}
+   </>
+  );
+}
+
+export default App;
